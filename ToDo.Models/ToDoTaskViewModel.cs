27576dcf6ffe2d484d2b7c9135858eabccb150a3
Md. Sajjat Hossen <sajjat.hossen@brainstation-23.com>
2024-05-24 17:40:18 +0600

    Remove UnnecessaryFiles

--- ToDo.Models/ToDoTaskViewModel.cs
+++ ToDo.Models/ToDoTaskViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToDo.Models
+﻿namespace ToDo.Models
 {
     public class ToDoTaskViewModel
     {
